14fa7d5c2da3788daac747f5c231173ff59ad0b3
hafizafarhapuruhita-sys <hafizafarhapuruhita@gmail.com>
2026-06-11 21:41:59 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Halo Hafiza!");
+const addButton = document.getElementById("addButton");
+const transactionName = document.getElementById("transactionName");
+const transactionAmount = document.getElementById("transactionAmount");
+const transactionList = document.getElementById("transactionList");
+
+addButton.addEventListener("click", function () {
+
+    const name = transactionName.value;
+    const amount = transactionAmount.value;
+
+    const newItem = document.createElement("li");
+
+    newItem.textContent = name + " - Rp" + amount;
+
+    transactionList.appendChild(newItem);
+
+});
