0739bf4f976754fbda9c8ef78c2b9d41f4fa3768
hafizafarhapuruhita-sys <hafizafarhapuruhita@gmail.com>
2026-06-11 22:14:40 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,19 @@ addButton.addEventListener("click", function () {
     name + " - Rp" + amount + " (" + category + ")";
 
     transactionList.appendChild(newItem);
+    const currentTotal =
+    document.getElementById("totalAmount");
+
+let totalText =
+    currentTotal.textContent;
+
+let currentValue =
+    Number(totalText.replace("Total Pengeluaran: Rp",""));
+
+currentValue =
+    currentValue + Number(amount);
+
+currentTotal.textContent = "Total Pengeluaran: Rp" + currentValue;
     transactionName.value = "";
     transactionAmount.value = "";
 
