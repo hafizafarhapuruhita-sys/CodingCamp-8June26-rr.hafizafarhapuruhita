e16c392c8085008604443681c58707506554f4db
hafizafarhapuruhita-sys <hafizafarhapuruhita@gmail.com>
2026-06-12 21:45:58 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,13 +11,6 @@ transactionAmount.addEventListener("input", function () {
     transactionAmount.value =
         Number(value).toLocaleString("id-ID");
 });
-const spendingLimit =
-    document.getElementById("spendingLimit");
-    spendingLimit.addEventListener("input", function () {
-    let value = spendingLimit.value;
-    value = value.replace(/\D/g, "");
-    spendingLimit.value = Number(value).toLocaleString("id-ID");
-});
 const transactionCategory = document.getElementById("transactionCategory");
 const transactionList = document.getElementById("transactionList");
 const spendingLimit = document.getElementById("spendingLimit");
